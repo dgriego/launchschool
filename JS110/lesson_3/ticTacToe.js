d075a32b877101fa289f28b112801ee985d9c4a1
Daniel Griego <eggomygriego@gmail.com>
2023-09-04 21:41:54 +0000

    update ticTacToe with series logic

--- JS110/lesson_3/ticTacToe.js
+++ JS110/lesson_3/ticTacToe.js
@@ -2,12 +2,13 @@ const readline = require('readline-sync');
 const EMPTY_SQUARE_MARKER = ' ';
 const HUMAN_MARKER = 'X';
 const COMPUTER_MARKER = 'O';
+const PLAYER = 'Player';
+const COMPUTER = 'Computer';
+const SERIES_MAX = 5;
+const SINGLE_GAME = 1;
+const YES = 'y';
 
 function displayBoard(board) {
-  console.clear();
-
-  console.log(`You are ${HUMAN_MARKER}. Computer is ${COMPUTER_MARKER}`);
-
   console.log('');
   console.log('     |     |');
   console.log(`  ${board['1']}  |  ${board['2']}  |  ${board['3']}  `);
@@ -21,6 +22,15 @@ function displayBoard(board) {
   console.log(`  ${board['7']}  |  ${board['8']}  |  ${board['9']}  `);
   console.log('     |     |');
   console.log('');
+}
+
+function displayBoardHeader(score, maxScore) {
+  console.clear();
+
+  console.log(`You are ${HUMAN_MARKER}. Computer is ${COMPUTER_MARKER}`);
+
+  console.log(maxScore);
+  if (maxScore > 1) displayScore(score);
 }
 
 function joinOr(arr, delimiter = ', ', orDelimiter = 'or') {
@@ -102,13 +112,13 @@ function detectWinner(board) {
       board[sq2] === HUMAN_MARKER &&
       board[sq3] === HUMAN_MARKER
     ) {
-      return 'Player';
+      return PLAYER;
     } else if (
       board[sq1] === COMPUTER_MARKER &&
       board[sq2] === COMPUTER_MARKER &&
       board[sq3] === COMPUTER_MARKER
     ) {
-      return 'Computer';
+      return COMPUTER;
     }
   }
 
@@ -119,31 +129,106 @@ function someoneWon(board) {
   return !!detectWinner(board);
 }
 
-while (true) {
-  let board = initializeBoard();
-  displayBoard(board);
-
-  while (true) {
-    displayBoard(board);
-
-    playerChoosesSquare(board);
-    if (someoneWon(board) || boardFull(board)) break;
-
-    computerChoosesSquare(board);
-    if (someoneWon(board) || boardFull(board)) break;
-  }
-
-  displayBoard(board);
-
-  if (someoneWon(board)) {
-    prompt(`${detectWinner(board)} won!`);
+function displayWinOrTie(winner) {
+  if (winner) {
+    prompt(`${winner} won!`);
   } else {
     prompt("It's a tie!");
   }
+}
 
+function isPlayingAgain() {
   prompt('Play again? (y or n)');
-  let answer = readline.question().toLowerCase()[0];
-  if (answer !== 'y') break;
+
+  return readline.question().toLowerCase()[0] === YES;
+}
+
+function runGameTurnLoop(board, score) {
+  const boardCopy = JSON.parse(JSON.stringify(board));
+
+  while (true) {
+    displayBoardHeader(score);
+    displayBoard(boardCopy);
+
+    playerChoosesSquare(boardCopy);
+    if (someoneWon(boardCopy) || boardFull(boardCopy)) break;
+
+    computerChoosesSquare(boardCopy);
+    if (someoneWon(boardCopy) || boardFull(boardCopy)) break;
+  }
+
+  return boardCopy;
+}
+
+function playSeriesBestOfFive() {
+  prompt('Do you want to play a series best of 5?');
+
+  return readline.question().toLowerCase()[0] === YES;
+}
+
+function displayScore(score) {
+  prompt('SCORE:');
+  prompt(`Player: ${score.x} Computer: ${score.o}`);
+}
+
+function updateScore(score, winner) {
+  if (winner === PLAYER) {
+    score.x += 1;
+  } else if (winner === COMPUTER) {
+    score.o += 1;
+  }
+}
+
+function detectSeriesWinner(score, maxScore) {
+  if (score.x === maxScore) {
+    return PLAYER;
+  } else if (score.o === maxScore) {
+    return COMPUTER;
+  }
+
+  return null;
+}
+
+function displaySeriesWinner(seriesWinner, maxScore) {
+  if (maxScore === 1) return;
+
+  prompt(`${seriesWinner} wins the series!`);
+}
+
+// keep track best of 5
+// for who wins, for each game Turn loop
+// increment the score by 1 depending on who won
+// after the turn loop
+// get who won that turn
+// display who won that round
+
+// Game Control Loop
+while (true) {
+  let board = initializeBoard();
+  let gameWinner = null;
+  let seriesWinner = null;
+  const score = { x: 0, o: 0 };
+  const maxScore = playSeriesBestOfFive() ? SERIES_MAX : SINGLE_GAME;
+  console.log(maxScore);
+
+  displayBoardHeader(score, maxScore);
+  displayBoard(board, score);
+
+  board = runGameTurnLoop(board, score);
+
+  gameWinner = detectWinner(board);
+  updateScore(score, gameWinner);
+  displayBoardHeader(score, maxScore);
+  displayBoard(board);
+  displayWinOrTie(gameWinner);
+
+  seriesWinner = detectSeriesWinner(score, maxScore);
+
+  if (!seriesWinner) continue;
+
+  displaySeriesWinner(seriesWinner, maxScore);
+
+  if (!isPlayingAgain()) break;
 }
 
 prompt('Thanks for playing Tic Tac Toe!');

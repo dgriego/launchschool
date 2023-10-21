e05dbea1d23089812a720b5e2e07d024ff937007
dgriego <daniel.griego@eyesopen.com>
2023-10-21 13:56:55 -0700

    refactor unlucky days

--- JS110/small_problems/unluckyDays.js
+++ JS110/small_problems/unluckyDays.js
@@ -1,21 +1,11 @@
-// INIT Date object for the year
-// loop through each month
-// get the 13th day of each month
-// check if that day is a friday
-// return the number of friday the 13ths
-
-
 function fridayThe13ths(year) {
-  const FRIDAY = 5;
-  let fridayThe13thsCount = 0;
+  return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].reduce((acc, month) => {
+    if ((new Date(`${year}-${month}-13 00:00`)).getDay() === 5) {
+      return acc + 1;
+    }
 
-  for (let idx = 1; idx <= 12; idx++) {
-    const date = new Date(`${year}-${idx}-13 00:00`);
-
-    if (date.getDay() === FRIDAY) fridayThe13thsCount += 1;
-  }
-
-  return fridayThe13thsCount;
+    return acc;
+  }, 0);
 }
 
 console.log(fridayThe13ths(1986) === 1);

898bc6d9b2d87a990cb6d88ecb5e15d4f0695a39
dgriego <daniel.griego@eyesopen.com>
2023-10-21 14:08:18 -0700

    improve unluckyDays

--- JS110/small_problems/unluckyDays.js
+++ JS110/small_problems/unluckyDays.js
@@ -1,6 +1,6 @@
 function fridayThe13ths(year) {
-  return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].reduce((acc, month) => {
-    if ((new Date(`${year}-${month}-13 00:00`)).getDay() === 5) {
+  return (Array(12).fill(1)).reduce((acc, _, month) => {
+    if ((new Date(year, month, 13)).getDay() === 5) {
       return acc + 1;
     }
 

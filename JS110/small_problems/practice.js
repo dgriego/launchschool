737f023a6542ec3160c10fee403303d34b505af4
Daniel Griego <sirmrgriego@Daniels-MacBook-Pro.local>
2024-11-25 15:58:11 -0800

    problem 13 @ 16min

--- JS110/small_problems/practice.js
+++ JS110/small_problems/practice.js
@@ -1,33 +1,66 @@
+// Create a function that takes two strings as arguments and returns true if some 
+// portion of the characters in the first string can be rearranged to match the 
+// characters in the second. Otherwise, the function should return false.
+
+// You may assume that both string arguments only contain lowercase alphabetic 
+// characters. Neither string will be empty.
+/*
+* P-
+*   - input: two strings
+*   - output: boolean
+*   so they just have to have the same letters essentially
+* . also same letter counts
+* . need a mechanism for making sure it's total is the asme
+* . 
+* 
+* . letter counts, match the numbers from both
+* . loop through the valid one requires that I make sure I don't recheck values
+*   I could have a tracked array that includes only valid or matched indexes
+*
+* E-
+*   - unscramble('ansucchlohlo', 'launchschool') === true
+*   - unscramble('phyarunstole', 'pythonrules') === true
+*   - unscramble('phyarunstola', 'pythonrules') === false
+*   - unscramble('boldface', 'coal') === true
+* . - 'aabbcc' 'abc'
+* D-
+*   - array
+*
+* A-
+*   tracked indexes arr
+    count = 0
+* .  
+* .  loop (idx)
+        second string char
+        inner loop
+           first string char
+          if 2nd === first
+             track index
+             count++
+*/
 const p = console.log;
-const eq = (arr1, arr2) => JSON.stringify(arr1) === JSON.stringify(arr2);
 
-function repeatedSubstring(str) {
-  let repeatCount = -Infinity;
-  let currentSet = ["", 0];
+function unscramble(str1, str2) {
+  const trackedIdx = []
+  let count = 0
 
-  for (let i = 0; i < str.length; i++) {
-    let count = 0;
-    const substr = str.slice(0, i + 1);
+  for (let i = 0; i < str2.length; i++) {
+    const charA = str2[i]
 
-    for (let x = 0; x < str.length; x++) {
-      const innerSubstr = str.slice(x, x + substr.length);
+    for (let x = 0; x < str1.length; x++) {
+      const charB = str1[x]
 
-      if (substr === innerSubstr) {
-        count += 1;
-      }
-
-      if (count > repeatCount && substr.length * count === str.length) {
-        repeatCount = count;
-        currentSet = [substr, count];
+      if (!trackedIdx.includes(x) && charA === charB) {
+        trackedIdx.push(x)
+        count++
       }
     }
   }
 
-  return currentSet;
+  return count === str2.length
 }
 
-p(eq(repeatedSubstring("xyzxyzxyz"), ["xyz", 3]));
-p(eq(repeatedSubstring("xyxy"), ["xy", 2]));
-p(eq(repeatedSubstring("xyz"), ["xyz", 1]));
-p(eq(repeatedSubstring("aaaaaaaa"), ["a", 8]));
-p(eq(repeatedSubstring("superduper"), ["superduper", 1]));
+p(unscramble('ansucchlohlo', 'launchschool') === true);
+p(unscramble('phyarunstole', 'pythonrules') === true);
+p(unscramble('phyarunstola', 'pythonrules') === false);
+p(unscramble('boldface', 'coal') === true);

b457aefb5eb2050045fa59bd312b1ba778b239df
Daniel Griego <sirmrgriego@Daniels-MacBook-Pro.local>
2024-11-25 16:16:18 -0800

    practice problem 14 @ 12 mins

--- JS110/small_problems/practice.js
+++ JS110/small_problems/practice.js
@@ -1,66 +1,65 @@
-// Create a function that takes two strings as arguments and returns true if some 
-// portion of the characters in the first string can be rearranged to match the 
-// characters in the second. Otherwise, the function should return false.
-
-// You may assume that both string arguments only contain lowercase alphabetic 
-// characters. Neither string will be empty.
 /*
-* P-
-*   - input: two strings
-*   - output: boolean
-*   so they just have to have the same letters essentially
-* . also same letter counts
-* . need a mechanism for making sure it's total is the asme
-* . 
-* 
-* . letter counts, match the numbers from both
-* . loop through the valid one requires that I make sure I don't recheck values
-*   I could have a tracked array that includes only valid or matched indexes
-*
-* E-
-*   - unscramble('ansucchlohlo', 'launchschool') === true
-*   - unscramble('phyarunstole', 'pythonrules') === true
-*   - unscramble('phyarunstola', 'pythonrules') === false
-*   - unscramble('boldface', 'coal') === true
-* . - 'aabbcc' 'abc'
-* D-
-*   - array
-*
-* A-
-*   tracked indexes arr
-    count = 0
-* .  
-* .  loop (idx)
-        second string char
-        inner loop
-           first string char
-          if 2nd === first
-             track index
-             count++
+Create a function that takes a single integer argument and returns the sum of all 
+the multiples of 7 or 11 that are less than the argument. If a number is a 
+multiple of both 7 and 11, count it just once.
+
+For example, the multiples of 7 and 11 that are below 25 are 7, 11, 14, 21, 
+and 22. The sum of these multiples is 75.
+
+If the argument is negative, return 0.
+
+P -
+
+return the sum of all the numbers below the given number that are multiples of 7 OR 11
+  - if it's a multiple of both, count it once (this means I could store the values and only have unique values)
+  - good use case for a set, since it's unique
+
+this could mean that I am able to loop up to (and including the number) and store
+values in a set
+
+1. determine a multiple
+2. loop up to number
+
+1. divide by a given number and compare the remainders (should be 0)
+    - loop can start at 7, since nothing below that will apply
+
+we can loop by 1 and check both numbers at once, or two separate loops and increment by 11 (and store in a set)
+
+
+set
+loop begin (start at 7, ++7)
+  7, 14, 21, 28
+  if multiple.
+  set.add
+
+loop begin (start at 11, ++11)
 */
 const p = console.log;
 
-function unscramble(str1, str2) {
-  const trackedIdx = []
-  let count = 0
+function sevenEleven(num) {
+  const multiples = []
 
-  for (let i = 0; i < str2.length; i++) {
-    const charA = str2[i]
+  if (num < 7) return 0
 
-    for (let x = 0; x < str1.length; x++) {
-      const charB = str1[x]
-
-      if (!trackedIdx.includes(x) && charA === charB) {
-        trackedIdx.push(x)
-        count++
-      }
+  for (let i = 7; i < num; i += 7) {
+    if (i % 7 === 0) {
+      multiples.push(i)
     }
   }
 
-  return count === str2.length
+  for (let i = 11; i < num; i += 11) {
+    if (!multiples.includes(i) && i % 11 === 0) {
+      multiples.push(i)
+    }
+  }
+
+  return multiples.reduce((a, b) => a + b, 0)
 }
 
-p(unscramble('ansucchlohlo', 'launchschool') === true);
-p(unscramble('phyarunstole', 'pythonrules') === true);
-p(unscramble('phyarunstola', 'pythonrules') === false);
-p(unscramble('boldface', 'coal') === true);
+p(sevenEleven(10) === 7);
+p(sevenEleven(11) === 7);
+p(sevenEleven(12) === 18);
+p(sevenEleven(25) === 75);
+p(sevenEleven(100) === 1153);
+p(sevenEleven(0) === 0);
+p(sevenEleven(-100) === 0);

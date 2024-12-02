770df38361899015181847633aba06abd689b3b4
dgriego <eggomygriego@gmail.com>
2024-12-02 15:09:00 -0800

    equalSumIndex @ 15 min

--- JS110/small_problems/practice.js
+++ JS110/small_problems/practice.js
@@ -1,61 +1,46 @@
 /*
+ *  index N - represents, all numbers with an index less than N
+ *  SUM to the same value as the numbers with an index greater than N
  *
- * - min int value that can be added to an array, where the SUM of that
- *   array equals the closest prime number that is greater than the current sum
+ *  grab middle point and sum to left and right of index
+ *  where the values are the same, return that middel point
  *
- *   sum of the array, we find the closest prime from that sum,
- *   we can subtract from the sum to the prime to determine the value
+ *  first point should be 1
+ *  0, 1, 2, 3
+ *  0 [1] 2 + 3
  *
- *   prime is divisible only by 1 and itself
- *   not divisible by 2 or 3
+ *  1, end just before the tail, length - 2, just have to do slice
  *
- *   1. sum of array
- *   2. from that sum, loop until we find the nearest prime
- *   3. once prime is located, subtract from sum to find min int
+ *  only have to control you iterator
  *
+ *  start
  *
- *   start
- *     reduce array
+ *  loop, end at array length, start at 0
+ *  if index is 0, sum is 0
  *
- *     loop starting at sum
- *        if val % 2 !== 0 and val % 3 !== 0
- *           break
- *
- *     min num = found prime - sum
+ *  if index is length - 1, sum is 0
  */
 
 const p = console.log;
 
-function isPrime(num) {
-  for (let i = 2; i < num; i++) {
-    if (num % i === 0) return false;
+function equalSumIndex(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    let sumLeft = 0;
+    let sumRight = 0;
+
+    sumLeft = nums.slice(0, i).reduce((a, b) => a + b, 0);
+    sumRight = nums.slice(i + 1).reduce((a, b) => a + b, 0);
+
+    if (sumLeft === sumRight) return i;
   }
 
-  return true;
+  return -1;
 }
+p(equalSumIndex([7, 99, 51, -48, 0, 4]) === 1);
+p(equalSumIndex([17, 20, 5, -60, 10, 25]) === 0);
+p(equalSumIndex([0, 2, 4, 4, 2, 3, 2]) === -1);
 
-function nearestPrimeSum(nums) {
-  const sum = nums.reduce((a, b) => a + b, 0);
-  let inc = 1;
-  let prime = 1;
-
-  while (true) {
-    prime = sum + inc;
-
-    if (isPrime(prime)) {
-      break;
-    }
-
-    inc++;
-  }
-
-  return prime - sum;
-}
-
-p(nearestPrimeSum([1, 2, 3]) === 1); // Nearest prime to 6 is 7
-p(nearestPrimeSum([5, 2]) === 4); // Nearest prime to 7 is 11
-p(nearestPrimeSum([1, 1, 1]) === 2); // Nearest prime to 3 is 5
-p(nearestPrimeSum([2, 12, 8, 4, 6]) === 5); // Nearest prime to 32 is 37
-//
-// // Nearest prime to 163 is 167
-p(nearestPrimeSum([50, 39, 49, 6, 17, 2]) === 4);
+// The following test case could return 0 or 3. Since we're
+// supposed to return the smallest correct index, the correct
+// return value is 0.
+p(equalSumIndex([0, 20, 10, -60, 5, 25]) === 0);

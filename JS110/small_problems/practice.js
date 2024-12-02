dfb884ac9078a63f8561b1073e62fd294f2c2338
dgriego <eggomygriego@gmail.com>
2024-12-02 15:22:06 -0800

    oddFellow @ 12mins

--- JS110/small_problems/practice.js
+++ JS110/small_problems/practice.js
@@ -1,46 +1,42 @@
 /*
- *  index N - represents, all numbers with an index less than N
- *  SUM to the same value as the numbers with an index greater than N
+ * dict to count
+ * then later check values
  *
- *  grab middle point and sum to left and right of index
- *  where the values are the same, return that middel point
+ * or, loop through each value, another loop inner that counts the rest
+ * finds first odd then ends
+ * don't thin i need tracker because it should end on odd value anyways.
  *
- *  first point should be 1
- *  0, 1, 2, 3
- *  0 [1] 2 + 3
+ * loop
+ *   inner loop
+ *     count if it mathes outer value
  *
- *  1, end just before the tail, length - 2, just have to do slice
- *
- *  only have to control you iterator
- *
- *  start
- *
- *  loop, end at array length, start at 0
- *  if index is 0, sum is 0
- *
- *  if index is length - 1, sum is 0
+ * return if odd (in scope of outer loop)
  */
 
 const p = console.log;
 
-function equalSumIndex(nums) {
+function oddFellow(nums) {
+  const seen = [];
+
   for (let i = 0; i < nums.length; i++) {
-    let sumLeft = 0;
-    let sumRight = 0;
+    const numA = nums[i];
+    let count = 1;
 
-    sumLeft = nums.slice(0, i).reduce((a, b) => a + b, 0);
-    sumRight = nums.slice(i + 1).reduce((a, b) => a + b, 0);
+    if (seen.includes(numA)) continue;
 
-    if (sumLeft === sumRight) return i;
+    nums.slice(i + 1).forEach((numB) => {
+      if (numA === numB) count++;
+    });
+
+    if (count % 2 !== 0) return numA;
+    seen.push(numA);
   }
 
-  return -1;
+  return 0;
 }
-p(equalSumIndex([7, 99, 51, -48, 0, 4]) === 1);
-p(equalSumIndex([17, 20, 5, -60, 10, 25]) === 0);
-p(equalSumIndex([0, 2, 4, 4, 2, 3, 2]) === -1);
 
-// The following test case could return 0 or 3. Since we're
-// supposed to return the smallest correct index, the correct
-// return value is 0.
-p(equalSumIndex([0, 20, 10, -60, 5, 25]) === 0);
+p(oddFellow([4]) === 4);
+p(oddFellow([7, 99, 7, 51, 99]) === 51);
+p(oddFellow([7, 99, 7, 51, 99, 7, 51]) === 7);
+p(oddFellow([25, 10, -6, 10, 25, 10, -6, 10, -6]) === -6);
+p(oddFellow([0, 0, 0]) === 0);

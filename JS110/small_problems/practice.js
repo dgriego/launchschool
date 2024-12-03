b3123a84ff94d2bcb7ca1c0670fec0e61b738f82
dgriego <eggomygriego@gmail.com>
2024-12-02 16:34:03 -0800

    rotateArray @ 7min

--- JS110/small_problems/practice.js
+++ JS110/small_problems/practice.js
@@ -2,15 +2,28 @@
  *
  */
 const p = console.log;
-function whatIsDifferent(nums) {
-  nums = nums.sort((a, b) => a - b);
 
-  if (nums[0] !== nums[1]) return nums[0];
-
-  return nums[nums.length - 1];
+function rotateArray(arr) {
+  if (!arr || !Array.isArray(arr)) return undefined;
+  if (!arr.length) return [];
+  const cp = arr.slice();
+  const rotated = [...cp.slice(1), cp[0]];
+  p(rotated);
+  return rotated;
 }
-p(whatIsDifferent([0, 1, 0]) === 1);
-p(whatIsDifferent([7, 7, 7, 7.7, 7]) === 7.7);
-p(whatIsDifferent([1, 1, 1, 1, 1, 1, 1, 11, 1, 1, 1, 1]) === 11);
-p(whatIsDifferent([3, 4, 4, 4]) === 3);
-p(whatIsDifferent([4, 4, 4, 3]) === 3);
+
+rotateArray([7, 3, 5, 2, 9, 1]); // [3, 5, 2, 9, 1, 7]
+rotateArray(["a", "b", "c"]); // ["b", "c", "a"]
+rotateArray(["a"]); // ["a"]
+rotateArray([1, "a", 3, "c"]); // ["a", 3, "c", 1]
+rotateArray([{ a: 2 }, [1, 2], 3]); // [[1, 2], 3, { a: 2 }]
+rotateArray([]); // []
+
+// return `undefined` if the argument is not an array
+p(rotateArray()); // undefined
+p(rotateArray(1)); // undefined
+
+// the input array is not mutated
+let array = [1, 2, 3, 4];
+rotateArray(array); // [2, 3, 4, 1]
+p(array); // [1, 2, 3, 4]
